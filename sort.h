5423b1ba39ccbead47b01c8e374febe3c579a30f
mohamed hajjoubi <mh041949@gmail.com>
2024-02-19 07:16:42 -0800

    Updatetask0

--- sort.h
+++ sort.h
@@ -34,8 +34,6 @@ void cocktail_sort_list(listint_t **list);
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
-void mergeSortTopDownSplit(int *array, int *array_sort, size_t index_beg, size_t index_end);
-void MergeSort(int *array, int *array_sort, size_t index_beg, size_t index_Middle, size_t index_end);
 void heap_sort(int *array, size_t size);
 void Heap(int *array, size_t size, size_t base, size_t i);
 void radix_sort(int *array, size_t size);
